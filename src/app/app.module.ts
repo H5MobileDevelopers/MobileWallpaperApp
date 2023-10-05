0a06a84f478c8dd440ffb5afaceb692f71c83cb1
haseeAmarathunga <haseeamarathunga@gmail.com>
2023-10-05 08:42:15 +0530

    Create components for Mobile Wallpaper App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './category/category.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, CategoryComponent, ImageComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
